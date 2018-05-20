e16347cc660db13dd2fb4573d163957a5e9842a5
Sebastian <sgarnicaq@unal.edu.co>
2018-05-20 18:43:22 -0500

    Templates added Template of main class and import between classes added

---
+++ src/Example/Example1.java
@@ -0,0 +1,25 @@
+package Example;
+
+import processing.core.*;
+import GameClasses.Class1;	// import of other classes
+
+public class Example1 extends PApplet{
+
+	public static void main (String[] args) {
+		PApplet.main("Example.Example1"); 	// The argument of this method must be "PackageName.ClassName"
+	}
+
+	@Override
+	public void settings () {
+		//fullScreen();
+		//size(640,480);
+	}
+
+	@Override
+	public void setup () {
+	}
+
+	@Override
+	public void draw () {
+	}
+}
---
+++ src/GameClasses/Class1.java
@@ -0,0 +1,18 @@
+package GameClasses;
+
+/**
+* All classes that are not the main one needs to have this template
+* All processing methods have to be called by p.
+*/
+
+import processing.core.*;
+
+public class Class1 {
+
+	PApplet p;
+
+	public Class1 (PApplet parent) {
+		this.p = parent;
+
+	}
+}
